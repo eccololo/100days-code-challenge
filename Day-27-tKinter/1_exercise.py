6d91e3ddcf7267490bae6ac9d67e1172dedf258b
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-05-29 13:43:19 +0200

    Day 27 - move function to the top.

--- Day-27-tKinter/1_exercise.py
+++ Day-27-tKinter/1_exercise.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 from tkinter.ttk import Label, Button, Entry
 
+
+def button_clicked():
+    user_input = entry.get()
+    label["text"] = user_input
+
+
 root = tk.Tk()
 root.title("My First Gui Program.")
 root.minsize(height=480, width=640)
@@ -9,12 +15,6 @@ root.minsize(height=480, width=640)
 label = Label(root, text="This is my label", font=("Arial", 25, "bold"))
 label.pack(expand=True)
 label["text"] = "This is a new text"
-
-
-def button_clicked():
-    user_input = entry.get()
-    label["text"] = user_input
-
 
 button = Button(text="My Button", command=button_clicked)
 button.pack(expand=True)
