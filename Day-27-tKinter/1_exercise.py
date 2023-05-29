5adc6683f94bd7f9bfdf44fb324c04482c914093
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-05-29 13:21:59 +0200

    Day 27 - add Entry input and Button to tKinter window.

--- Day-27-tKinter/1_exercise.py
+++ Day-27-tKinter/1_exercise.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter.ttk import Label
+from tkinter.ttk import Label, Button, Entry
 
 root = tk.Tk()
 root.title("My First Gui Program.")
@@ -8,4 +8,18 @@ root.minsize(height=480, width=640)
 # Labels
 label = Label(root, text="This is my label", font=("Arial", 25, "bold"))
 label.pack(expand=True)
+label["text"] = "This is a new text"
+
+
+def button_clicked():
+    user_input = entry.get()
+    label["text"] = user_input
+
+
+button = Button(text="My Button", command=button_clicked)
+button.pack(expand=True)
+
+entry = Entry(width=30)
+entry.pack(expand=True)
+
 root.mainloop()
