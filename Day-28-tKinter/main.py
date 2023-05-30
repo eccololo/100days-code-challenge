d8abd8cae50ddab0d14399a893e829a1c76d53b3
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-05-30 15:22:55 +0200

    Day 28 - add label and start button to Pomodor app.

--- Day-28-tKinter/main.py
+++ Day-28-tKinter/main.py
@@ -12,7 +12,10 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+def start():
+    pass
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
@@ -21,10 +24,16 @@ root = Tk()
 root.title("Pomodoro App")
 root.config(pady=50, padx=100, bg=YELLOW)
 
+label_1 = Label(text="Timer", font=(FONT_NAME, 25, "bold"), background=YELLOW, foreground=GREEN)
+label_1.grid(row=0, column=1)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="./tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+start_btn = Button(text="Start", command=start)
+start_btn.grid(row=2, column=0)
 
 root.mainloop()
