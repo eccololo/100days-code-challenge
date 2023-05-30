01628d6279de2ba1f83de84c453dd54bffd3a471
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-05-30 15:28:35 +0200

    Day 28 - add style to start button and reset button.

--- Day-28-tKinter/main.py
+++ Day-28-tKinter/main.py
@@ -1,23 +1,31 @@
 from tkinter import *
 from tkinter.ttk import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
+BLUE = "#11009E"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+def reset():
+    pass
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 def start():
     pass
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
 root = Tk()
@@ -33,7 +41,13 @@ canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
-start_btn = Button(text="Start", command=start)
+style = Style()
+style.configure('W.TButton', font=(FONT_NAME, 11, 'bold'), foreground=BLUE)
+
+start_btn = Button(text="Start", command=start, style="W.TButton")
 start_btn.grid(row=2, column=0)
+
+start_btn = Button(text="Reset", command=reset, style="W.TButton")
+start_btn.grid(row=2, column=2)
 
 root.mainloop()
