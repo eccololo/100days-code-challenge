e2d37ae28c49a122d30fc9b4a151222c74d287d9
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-05-30 16:11:01 +0200

    Day 28 - add test timer to Pomodoro app.

--- Day-28-tKinter/main.py
+++ Day-28-tKinter/main.py
@@ -21,8 +21,11 @@ def reset():
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
-def start():
-    pass
+def count_down(count):
+    canvas.itemconfigure(timer_text, text=count)
+    if count > 0:
+        print(count)
+        root.after(1000, count_down, count - 1)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -38,13 +41,15 @@ label_1.grid(row=0, column=1)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="./tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
+
+count_down(5)
 
 style = Style()
 style.configure('Action.TButton', font=(FONT_NAME, 11, 'bold'), foreground=BLUE)
 
-start_btn = Button(text="Start", command=start, style="Action.TButton")
+start_btn = Button(text="Start", command=count_down, style="Action.TButton")
 start_btn.grid(row=2, column=0)
 
 start_btn = Button(text="Reset", command=reset, style="Action.TButton")
