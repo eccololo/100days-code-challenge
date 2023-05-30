4f899c8e4d2b1ccf13530f3d03f38275e0487170
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-05-30 15:15:14 +0200

    Day 28 - add pomodoro image canvas to Pomodoro app.

--- Day-28-tKinter/main.py
+++ Day-28-tKinter/main.py
@@ -1,4 +1,5 @@
-
+from tkinter import *
+from tkinter.ttk import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +17,14 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+root = Tk()
+root.title("Pomodoro App")
+root.config(pady=50, padx=100, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="./tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+root.mainloop()
