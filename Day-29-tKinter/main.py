4593691c8ed5ccaec7012482819b24c51c9f8988
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-29 12:34:32 +0200

    Add app logo on the center on the app window.

--- Day-29-tKinter/main.py
+++ Day-29-tKinter/main.py
@@ -2,6 +2,18 @@ from tkinter import *
 from tkinter.ttk import *
 from playsound import playsound
 
+root = Tk()
+root.title("Desktop Pass Manager")
+root.geometry("280x290")
+root.config(pady=20, padx=20)
+
+canvas = Canvas(root, width=200, height=200)
+logo_image = PhotoImage(file="./logo.png")
+canvas.create_image(120, 120, image=logo_image)
+canvas.grid(column=0, row=0)
+
+root.mainloop()
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
