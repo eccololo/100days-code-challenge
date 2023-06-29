d8cd599a90bc7df91ae0b896a8b6ff3e56308f7b
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-06-29 13:10:23 +0200

    Add Password Manager project UI.

--- Day-29-tKinter/main.py
+++ Day-29-tKinter/main.py
@@ -4,18 +4,49 @@ from playsound import playsound
 
 root = Tk()
 root.title("Desktop Pass Manager")
-root.geometry("280x290")
-root.config(pady=20, padx=20)
+root.geometry("520x400")
+root.config(pady=50, padx=50)
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def gen_pass():
+    pass
+
+def add_pass():
+    pass
+
+# ---------------------------- UI SETUP ------------------------------- #
 
 canvas = Canvas(root, width=200, height=200)
 logo_image = PhotoImage(file="./logo.png")
-canvas.create_image(120, 120, image=logo_image)
-canvas.grid(column=0, row=0)
+canvas.create_image(100, 100, image=logo_image)
+canvas.grid(column=1, row=0)
+
+label_www = Label(root, text="Website: ")
+label_www.grid(row=1, column=0)
+
+label_login = Label(root, text="Email / Username: ")
+label_login.grid(row=2, column=0)
+
+label_pass = Label(root, text="Password: ")
+label_pass.grid(row=3, column=0)
+
+entry_www = Entry(root, width=48)
+entry_www.grid(row=1, column=1, columnspan=2)
+
+entry_login = Entry(root, width=48)
+entry_login.grid(row=2, column=1, columnspan=2)
+
+entry_pass = Entry(root, width=34)
+entry_pass.grid(row=3, column=1)
+
+btn_pass = Button(root, text="Generate Pass", command=gen_pass)
+btn_pass.grid(row=3, column=2)
+
+btn_add = Button(root, text="Add", command=add_pass, width=48)
+btn_add.grid(row=4, column=1, columnspan=2)
 
 root.mainloop()
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
-# ---------------------------- UI SETUP ------------------------------- #
