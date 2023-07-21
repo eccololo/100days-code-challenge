6e9810e1274ac42a80cc989f8eb1ec1aecc77ec8
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-07-21 16:27:25 +0200

    Add boilerplate tKinter starting code to Flash Cards project.

--- Day-31-FlashCards/main.py
+++ Day-31-FlashCards/main.py
@@ -4,3 +4,13 @@ from tkinter import messagebox
 from playsound import playsound
 import random
 import csv
+
+
+DATA_FILE_PATH = "assets/data/300_italian_polish_most_common_words.csv"
+
+root = Tk()
+root.title("Flash Cards App")
+root.geometry("620x500")
+root.config(pady=50, padx=50)
+
+root.mainloop()
