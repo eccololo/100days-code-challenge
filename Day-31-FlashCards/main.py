42c438824f9024facea36ad79d9ff9ea70849c9d
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-07-26 10:11:39 +0200

    Add feature. Now the data from DB file are showing on front of the flash card.

--- Day-31-FlashCards/main.py
+++ Day-31-FlashCards/main.py
@@ -1,3 +1,8 @@
+# TODO:
+#    2. Zrobić timer tak aby po 5 sekundach karta flipowala sie na odpowiedz. Dane pobierane z data-set.
+#    3. Zrobic tak aby po flipie na odpowiedz timer przestawal odliczac i program czekal na odpowiedz usera.
+
+
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import messagebox
@@ -5,14 +10,46 @@ from playsound import playsound
 import random
 import csv
 import inspect
+import sys
 
 DATA_FILE_PATH = "assets/data/300_italian_polish_most_common_words.csv"
+DATA_SET = None
 FONT_NAME = "Courier"
 WINDOW_WIDTH = 720
 WINDOW_HEIGHT = 550
 
 
+# ===================== Data ====================
+def return_question_answer_from_data_set():
+    """This function gets next question and answer from data-set file and returns it."""
+    csvfile = None
+    reader = None
+    data_set = {}
+    counter = 1
+    try:
+        csvfile = open(DATA_FILE_PATH, "r", encoding='utf-8')
+        reader = csv.reader(csvfile, delimiter=',')
+    except FileNotFoundError:
+        messagebox.showinfo("Error.", "Sorry, but DB file with answers and question was not found.\nContact developer.")
+        sys.exit()
+    finally:
+        for row in csvfile:
+            question = row.split(",")[0].replace("\n", "")
+            answer = row.split(",")[1].replace("\n", "")
+            data_set[counter] = {"question": question, "answer": answer}
+            counter += 1
+
+        csvfile.close()
+        return data_set
+
+
 # ===================== Utilities ===============
+def clear_canvas_writing_field(title, q_a_txt):
+    """This function clear what is written on canvas."""
+    canvas.delete(title)
+    canvas.delete(q_a_txt)
+
+
 def center_the_project_window(w_root):
     """This function centers app window of the center of the screen when it is open. Code taken from
     StackOver Flow."""
@@ -32,11 +69,26 @@ def center_the_project_window(w_root):
     w_root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
 
+# ===================== FUNCTIONALITY ===============
+def show_next_question():
+    """This function shows on GUI next question. Takes data from data-set."""
+
+    subject_front = DATA_SET[1]["question"]
+    subject_back = DATA_SET[1]["answer"]
+    question_no = 2
+    question = DATA_SET[question_no]["question"]
+    answer = DATA_SET[question_no]["answer"]
+    canvas.create_text(290, 100, text=f"{subject_front}", fill="black", font=(FONT_NAME, 30, "bold"))
+    canvas.create_text(300, 210, text=f"{question}", fill="black", font=(FONT_NAME, 55, "bold"))
+
+
 root = Tk()
 root.title("Flash Cards App")
 center_the_project_window(root)
 root.config(pady=40, padx=40)
 root.configure(bg='#B1DDC6')
+
+DATA_SET = return_question_answer_from_data_set()
 
 # ======================= UI ===========================
 canvas = Canvas(root, width=625, height=395, bg='#B1DDC6', highlightthickness=0)
@@ -46,7 +98,6 @@ canvas.grid(column=0, row=0, columnspan=2)
 title = canvas.create_text(290, 100, text="Italian", fill="black", font=(FONT_NAME, 30, "bold"))
 question_word = canvas.create_text(300, 210, text="Question", fill="black", font=(FONT_NAME, 55, "bold"))
 
-
 false_image = PhotoImage(file="./assets/images/wrong.png")
 false_btn = Button(image=false_image)
 # false_btn.config(highlightthickness=0, borderwidth=0)
@@ -55,4 +106,8 @@ false_btn.grid(row=1, column=0)
 right_image = PhotoImage(file="./assets/images/right.png")
 right_btn = Button(image=right_image)
 right_btn.grid(row=1, column=1)
+
+clear_canvas_writing_field(title, question_word)
+show_next_question()
+
 root.mainloop()
--- Day-31-FlashCards/util_1.py
+++ Day-31-FlashCards/util_1.py
@@ -1,11 +1,22 @@
-with open("./assets/it_words_source_50k.txt", "r") as f_1:
-    data = f_1.readlines()
+# with open("./assets/it_words_source_50k.txt", "r") as f_1:
+#     data = f_1.readlines()
+#
+# output_data = []
+# for item in data:
+#     output_data.append(item.split(" ")[0])
+#
+# with open("./assets/it_words_50k.txt", "w") as f_2:
+#     for word in output_data:
+#         text = f"{word}\n"
+#         f_2.write(text)
 
-output_data = []
-for item in data:
-    output_data.append(item.split(" ")[0])
+some_list = [["a", "b"], ["c", "d"], ["e", "f"]]
+counter = 1
+data_set = {}
 
-with open("./assets/it_words_50k.txt", "w") as f_2:
-    for word in output_data:
-        text = f"{word}\n"
-        f_2.write(text)
+for row in some_list:
+    data_set[counter] = {"question": row[0]}
+    data_set[counter] = {"answer": row[1]}
+    counter += 1
+
+print(data_set)
