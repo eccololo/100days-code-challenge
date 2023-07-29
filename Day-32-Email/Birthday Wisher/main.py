b3f7512fb9adbad98367dbaaf1f5baba3ca67fd4
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-07-29 13:29:21 +0200

    Add function get_birthday_data that returns email data from csv if current day and month are the same as in csv file.

--- Day-32-Email/Birthday Wisher/main.py
+++ Day-32-Email/Birthday Wisher/main.py
@@ -6,18 +6,26 @@ import pandas as pd
 BIRTHDAY_FILE = "./birthdays.csv"
 
 
-def is_someones_birthday(file_path):
+def get_birthday_data(file_path):
     """This function returns True if there is someones birthday."""
     day = dt.datetime.now().day
     month = dt.datetime.now().month
-    df = pd.read_csv(file_path, delimiter='\t', header=None)
-    return df
+
+    df = pd.read_csv(file_path)
+    for index, row in df.iterrows():
+        df_day = row['day']
+        df_month = row['month']
+        df_name = row['name']
+        df_year = row['year']
+        df_email = row['email']
+
+        if day == df_day and month == df_month:
+            return df_name, df_email, df_year
 
 
 email_from = "mateusz.hyla.ff@gmail.com"
 password_from = "<app_password>"
-is_birthday = is_someones_birthday(BIRTHDAY_FILE)
-print(is_birthday)
+email_data = get_birthday_data(BIRTHDAY_FILE)
 
 # with smtplib.SMTP("smtp.gmail.com") as conn:
 #     conn.starttls()
