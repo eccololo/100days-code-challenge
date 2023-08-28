6fe5e9493f7abe1405972461a5eee4ca1ddb09d4
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-08-28 14:20:47 +0200

    Day 33 - APIs - experiments on sunset and sunrise API.

--- Day-33-APIs/1_exercise.py
+++ Day-33-APIs/1_exercise.py
@@ -1,11 +1,35 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 50.064651
+MY_LONG = 19.944981
+
+# ISS LOCATION API
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+# longitude = data['iss_position']['longitude']
+# latitude = data['iss_position']['latitude']
+#
+# iss_position = (longitude, latitude)
+# print(iss_position)
+
+# SUN RISE SUN SET API
+
+# Cords for Kraków, Poland
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 
 data = response.json()
-longitude = data['iss_position']['longitude']
-latitude = data['iss_position']['latitude']
-
-iss_position = (longitude, latitude)
-print(iss_position)
+sunrise_hour = data['results']['sunrise'].split("T")[1].split(":")[0]
+sunset_hour = data['results']['sunset'].split("T")[1].split(":")[0]
+now_datetime_hour = datetime.now().hour
+print(sunrise_hour)
+print(sunset_hour)
+print(now_datetime_hour)
