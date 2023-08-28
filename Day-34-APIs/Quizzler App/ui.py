0adbbe417172af5332e03f267c89c8d77856f6d3
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-08-28 16:50:18 +0200

    Day 34 - APIs - Add canvas with question text to Quizzlet app.

--- Day-34-APIs/Quizzler App/ui.py
+++ Day-34-APIs/Quizzler App/ui.py
@@ -11,18 +11,16 @@ class QuizInterface:
     def __init__(self):
         self.root = Tk()
         self.root.title("Quizzlet")
-        self.root.config(pady=20, padx=20)
-        self.root.configure(bg=THEME_COLOR)
+        self.root.config(pady=20, padx=20, bg=THEME_COLOR)
         self.root.geometry("400x500")
 
         self.score_label = Label(self.root, text="Score: 0", background=THEME_COLOR,
                                  font=("Arial", 12, "bold"), foreground="white")
         self.score_label.grid(row=0, column=1, sticky='E', pady=10)
 
-        self.question_label = Label(self.root, text="Question 1", background="white",
-                                 font=("Arial", 20, "italic"), foreground="black")
-        self.question_label.configure(padding=(110, 100))
-        self.question_label.grid(row=1, column=0, columnspan=2, pady=5)
+        self.question_canvas = Canvas(width=360, height=250, bg="white", highlightthickness=0)
+        self.question_text = self.question_canvas.create_text(90, 70, text="Question 1", fill="black", font=("Arial", 20, "italic"))
+        self.question_canvas.grid(row=1, column=0, columnspan=2, pady=5)
 
         self.true_btn_img = PhotoImage(file="./images/true.png")
         self.true_btn = Button(text="Send", width=13, image=self.true_btn_img,
