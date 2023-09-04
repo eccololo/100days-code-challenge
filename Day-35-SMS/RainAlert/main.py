bb9e6e0d13a2c774b26e5abdd15f220eadbdac99
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-09-04 13:01:58 +0200

    Day 35 - Rain Alert - creating rain alert app.

--- Day-35-SMS/RainAlert/main.py
+++ Day-35-SMS/RainAlert/main.py
@@ -1,1 +1,23 @@
+import requests
+
+api_endpoint_standard = "https://api.openweathermap.org/data/2.5/weather"
 api_key = ""
+
+parameters_3_days = {
+    "lat": 50.064651,
+    "lon": 19.944981,
+    "appid": api_key
+}
+
+parameters_standard = {
+    "q": "Krakow,pl",
+    "appid": api_key
+}
+
+response = requests.get(api_endpoint_standard, params=parameters_standard)
+response.raise_for_status()
+
+weather_code = response.json()['weather'][0]['id']
+if weather_code < 700:
+    print("Please, take with you an umbrella.")
+
