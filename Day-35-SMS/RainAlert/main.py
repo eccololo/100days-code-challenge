a24f6c9aaa9ab2037164c7f23f5d8e9cd7d5f527
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-09-06 16:24:01 +0200

    Day 35 - Rain Alert - connecting with Twilio API to send SMS alerts of rain.

--- Day-35-SMS/RainAlert/main.py
+++ Day-35-SMS/RainAlert/main.py
@@ -1,8 +1,14 @@
 import requests
+from twilio.rest import Client
+
+twilio_account_sid = '<account_sid>'
+twilio_auth_token = '<api_key>'
+twilio_client = Client(twilio_account_sid, twilio_auth_token)
 
 api_endpoint_standard = "https://api.openweathermap.org/data/2.5/weather"
-api_key = ""
+api_key = "<api_key>"
 
+# Krakow, PL
 parameters_3_days = {
     "lat": 50.064651,
     "lon": 19.944981,
@@ -18,6 +24,12 @@ response = requests.get(api_endpoint_standard, params=parameters_standard)
 response.raise_for_status()
 
 weather_code = response.json()['weather'][0]['id']
-if weather_code < 700:
-    print("Please, take with you an umbrella.")
+if weather_code <= 800:
+    message = twilio_client.messages.create(
+        from_='<tel_from>',
+        to='<tel_to>',
+        body="Please, take with you an umbrella today ☂️."
+    )
+
+    print(message.status)
 
