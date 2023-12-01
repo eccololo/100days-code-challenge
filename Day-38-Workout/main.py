d43e98728385d8148354a68dcf865aeb26ec103d
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-12-01 13:40:23 +0100

    Day 38 - Workout App - Connecting to connect to API.

--- Day-38-Workout/main.py
+++ Day-38-Workout/main.py
@@ -1,7 +1,23 @@
 import os
+import requests
 
 APP_ID = "749076b3"
 API_KEY = os.environ.get("NUTRITIONIX_API_KEY")
+NUTRI_END_POINT = 'https://trackapi.nutritionix.com/v2/natural/exercise'
+
+request_parameters = {
+    'method': 'POST',
+    'url': NUTRI_END_POINT,
+    'headers': {
+        'Content-Type': 'application/json',
+        'x-app-id': APP_ID,
+        'x-app-key': API_KEY
+    },
+    'body': {
+        'query': "doing yoga for 45 minutes"
+    }
+}
 
 if __name__ == "__main__":
-    pass
+    response = requests.post(url=NUTRI_END_POINT, json=request_parameters)
+    print(response.status_code)
