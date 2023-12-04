d4efc3f4b40a91d77b8ef33efb1cb4507ece47da
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-12-04 14:45:37 +0100

    Day 38 - Workout App - Refactoring main.py.

--- Day-38-Workout/main.py
+++ Day-38-Workout/main.py
@@ -2,6 +2,51 @@ import os
 import requests
 import datetime
 
+
+def get_current_date_and_time():
+    """Returning current time and date as a tuple."""
+    time = datetime.datetime.now().time().strftime("%H:%M:%S")
+    date = datetime.datetime.now().strftime("%Y/%m/%d")
+    return date, time
+
+
+def get_data_from_nutritionix_as_json(endpoint, params, heads):
+    """Returning JSON data from Nutritionix API."""
+    return requests.post(url=endpoint, json=params, headers=heads).json()
+
+
+def get_sheety_payload_as_dict(response):
+    """Creating a payload dict to send to Sheety and to Save data to Google Sheets."""
+    dur_min = response["exercises"][0]["duration_min"]
+    cals = response["exercises"][0]["nf_calories"]
+    execs = str(response["exercises"][0]["name"]).title()
+    date, time = get_current_date_and_time()
+
+    temp_dict = {
+        'Date': date,
+        'Time': time,
+        'Duration': dur_min,
+        'Exercise': execs,
+        'Calories': cals
+    }
+
+    return temp_dict
+
+
+def send_data_to_google_sheets_and_return_status_code_and_text(endpoint, params):
+    """Sending data to Google Sheets and returning server response status code."""
+    resp = requests.post(
+        url=endpoint,
+        json=params,
+        auth=(
+            os.environ["ENV_SHEETY_USERNAME"],
+            os.environ["ENV_SHEETY_PASSWORD"],
+        ))
+
+    return resp.status_code, resp.text
+
+
+
 # ===== NUTRITIONIX API =======
 
 GENDER = "male"
@@ -44,35 +89,24 @@ sheety_params = {
 }
 
 if __name__ == "__main__":
-    current_time = datetime.datetime.now().time().strftime("%H:%M:%S")
-    current_date = datetime.datetime.now().strftime("%Y/%m/%d")
+    current_time = get_current_date_and_time()[1]
+    current_date = get_current_date_and_time()[0]
 
-    response = requests.post(url=NUTRI_END_POINT, json=request_parameters, headers=headers).json()
+    response = get_data_from_nutritionix_as_json(NUTRI_END_POINT, request_parameters, headers)
 
-    duration_min = response["exercises"][0]["duration_min"]
-    calories = response["exercises"][0]["nf_calories"]
-    exercise = str(response["exercises"][0]["name"]).title()
-
-    sheety_params[GOOGLE_SHEET_NAME]['Date'] = current_date
-    sheety_params[GOOGLE_SHEET_NAME]['Time'] = current_time
-    sheety_params[GOOGLE_SHEET_NAME]['Duration'] = duration_min
-    sheety_params[GOOGLE_SHEET_NAME]['Exercise'] = exercise
-    sheety_params[GOOGLE_SHEET_NAME]['Calories'] = calories
+    sheety_params[GOOGLE_SHEET_NAME] = get_sheety_payload_as_dict(response)
 
     # FIXME:
     #    1. Data are sent but doesn't saved in Google Sheets - Why?
 
-    response = requests.post(
-        url=SHEETY_ENDPOINT,
-        json=sheety_params,
-        auth=(
-            os.environ["ENV_SHEETY_USERNAME"],
-            os.environ["ENV_SHEETY_PASSWORD"],
-        ))
+    success = send_data_to_google_sheets_and_return_status_code_and_text(SHEETY_ENDPOINT, sheety_params)
 
-    if response.status_code != 200:
+    if success[0] != 200:
         print("Ups. Something went wrong.")
-        print(response.status_code, response.text)
     else:
         print("Data added to your Google Sheet successfully.")
-        print(response.status_code, response.text)
+
+    print("*" * 30)
+    print("[Analysis]:")
+    print(f"Server response status code: {success[0]}")
+    print(f"Server response message: {success[1]}")
